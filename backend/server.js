d10cdfaa55d42b80a537539d9be3cd2aa4ca4437
Rebecca <rebecca_nhan@rogers.com>
2023-05-17 16:53:32 -0400

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
-const port = 5000
+const connectDB = require('./config/db')
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+connectDB()
+
 const app = express()
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
