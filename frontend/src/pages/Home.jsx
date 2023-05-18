3f8849cc03c6767d9ab5e268ce2f4b9b44fb78a5
Rebecca <rebecca_nhan@rogers.com>
2023-05-18 15:58:59 -0400

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Header from '../components/Header'
+import './Home.css';
 
 function Home() {
     return (
-        <div className="home">
+        <div>
             <Header/>
-            <h1>Home</h1>
+            <div className="home">
+                <h1>IPA Practice for LIN101</h1>
+                <br/>
+                <p>You can use these three exercises to help you memorize the IPA symbols. These are the symbols that we are to study in LIN101, and we are using Canadian English.</p>
+                <ul>
+                    <li><a href="/articulation">Articulation</a>: Use this exercise to help you remember the phonetic properties of vowels and consonants. </li>
+                    <br/>
+                    <li><a href="/ipa-to-english">IPA to English</a>: Use this exercise to practice translating IPA words to English. You will be given a word in IPA symbols and will be given choices to translate the word into English</li>
+                    <br/>
+                    <li><a href="/english-to-ipa">English to IPA</a>: Use this exercise to practice translating English to IPA. You will be given a word in English, and will be given choices to translate the word into IPA.</li>
+                </ul>
+            </div>
         </div>
     )
 }
