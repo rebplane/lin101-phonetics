f0e233456397e873b3cfaf21fe4b139f499fc2d3
Rebecca <rebecca_nhan@rogers.com>
2023-05-30 15:38:40 -0400

    Added randomizer for backend and started frontend template for ipa to english exercise

--- backend/controllers/wordController.js
+++ backend/controllers/wordController.js
@@ -1,15 +1,34 @@
 const asyncHandler = require('express-async-handler')
 const Word = require('../models/wordModel')
 
-// @route GET /api/symbol
+// @route GET /api/word/
 const getRandomWord = asyncHandler(async(req, res) => {
-    // Get one random document from the collection
+    // Get one random word from the collection, along with a list of the words in randomized order
     const word = await Word.aggregate(
         [ { $sample: { size: 1 } } ]
-     )
+    )
+
+    var word_obj = word[0]
+
+    var word_list = [word_obj.english_word, word_obj.word1, word_obj.word2, word_obj.word3]
+    shuffle(word_list)
+
+    word_obj.word_list = word_list
 
     res.status(200).json(word)
 })
+
+// Takes a list of strings and shuffles the order
+function shuffle(word_list) {
+    var swap, temp;
+
+    for (let i = word_list.length - 1; i > 0; i--) {
+        swap = Math.floor(Math.random() * (i+1))
+        temp = word_list[i]
+        word_list[i] = word_list[swap]
+        word_list[swap] = temp
+    }
+}
 
 module.exports = {
     getRandomWord,
--- frontend/src/pages/IPAToEnglish.jsx
+++ frontend/src/pages/IPAToEnglish.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from '../components/Header'
+import Button from 'react-bootstrap/Button';
 
 function IPAToEnglish() {
     return (
-        <div className="ipa-to-english">
+        <div>
             <Header/>
-            <h1>IPA to English</h1>
-            <p>Coming soon</p>
+            <div className="ipa-to-english">
+                <h1><u>IPA to English</u></h1>
+                <p>Choose the English word that corresponds to the IPA transcription</p>
+                <h2>Word goes here</h2>
+                <Button variant="secondary" size="md"> tasdfasdf </Button>
+                <Button variant="secondary" size="md"> asdasdflj </Button>
+                <Button variant="secondary" size="md"> Next </Button>
+                <Button variant="secondary" size="md"> Next </Button>
+            </div>
         </div>
     )
 }
